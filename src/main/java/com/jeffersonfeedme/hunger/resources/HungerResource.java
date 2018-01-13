3eb474c0e2451281632a6467ae14dc4a470d22c8
Jeff Hansen <dscheffy@gmail.com>
2018-01-13 14:51:17 +0530

    Add some basic logging of selections

--- src/main/java/com/jeffersonfeedme/hunger/resources/HungerResource.java
+++ src/main/java/com/jeffersonfeedme/hunger/resources/HungerResource.java
@@ -8,18 +8,25 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Optional;
 
 @Path("/hunger")
 @Produces(MediaType.APPLICATION_JSON)
 public class HungerResource {
 
+	private static Logger logger = LoggerFactory.getLogger(HungerResource.class);
+	
     public HungerResource() {
     }
 
     @GET
     @Timed
-    public Choice feedMe(@QueryParam("lastChoice") Optional<String> lastChoice) {
+    public Choice feedMe(@QueryParam("chosen") Optional<String> chosen) {
+    	    logger.info("chosen=" + chosen.get());
         return new Choice("0000105.jpg", "0000129.jpg");
     }
 }
