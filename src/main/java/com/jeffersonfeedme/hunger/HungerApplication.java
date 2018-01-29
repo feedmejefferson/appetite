b058906a09e4ed3bf6f227dca325db57c92b6281
Jeff Hansen <dscheffy@gmail.com>
2018-01-29 17:03:41 +0530

    Add CORS header and redesign build for v0.2
    In order to simplify the integration and deployment process, we wanted
    to separate the static content (web pages and javascript) from the
    dynamic backend (this dropwizard app). In the original release we got
    around having to deal with Cross Origin Resource Sharing by running both
    the webserver and the hunger web service on the same server AND having
    the webserver act as a proxy to the web service for any external
    requests. This worked, but hosting our static web pages from an ec2
    micro instance seemed to be a little bit slower than letting github do
    it for free with github pages. It also meant that deployment of changes
    to the web pages was more involved than simply pushing those changes to
    the master branch of the github repo.
    
    Now that we've decoupled the two sets of resources, we can also simplify
    the integration and deployment step which means we can bring the docker
    build back into this repository rather than having it managed from a
    separate integration repo. This may change again in the future, but it
    works for now.

--- src/main/java/com/jeffersonfeedme/hunger/HungerApplication.java
+++ src/main/java/com/jeffersonfeedme/hunger/HungerApplication.java
@@ -1,5 +1,12 @@
 package com.jeffersonfeedme.hunger;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
 import com.jeffersonfeedme.hunger.resources.HungerResource;
 
 import io.dropwizard.Application;
@@ -26,6 +33,21 @@ public class HungerApplication extends Application<HungerConfiguration> {
     public void run(final HungerConfiguration configuration, final Environment environment) {
         final HungerResource resource = new HungerResource(configuration.getCafeteria());
         environment.jersey().register(resource);
+        configureCors(environment);
     }
 
+    private void configureCors(Environment environment) {
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+    }
 }
