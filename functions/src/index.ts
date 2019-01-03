29c55471f4f6a38db7c2ff0b05d686a0b46d9f50
Jeff Hansen <dscheffy@gmail.com>
2019-01-03 08:02:29 +0000

    Rewrite original java logic in Typescript

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,29 @@
 import * as functions from 'firebase-functions';
+//import * as cors from 'cors';
+import { IMAGES } from './images';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+//const corsHandler = cors({origin: true});
+const cors = require('cors')({origin: true});
+
+function getSession(): number {
+  return Math.floor(Math.random() * 2147483647);
+}
+function getRandomFood(): number {
+  return Math.floor(Math.random() * IMAGES.length);
+}
+export const hunger = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    console.log(request.url);
+    let session: number = request.query.searchSession;
+    if (session == null) {
+      console.log("assigning a new session");
+      session = getSession(); 
+    }
+    const a=getRandomFood();
+    const b=getRandomFood();
+    response.json({"searchSession":session,"a":IMAGES[a],"b":IMAGES[b]});
+  });
+});
+
