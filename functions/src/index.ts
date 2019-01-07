85e40b219ad420fe9ff29ea5986f39dd0235bdde
Jeff Hansen <dscheffy@gmail.com>
2019-01-07 15:33:21 +0000

    Add a stratified model/strategy
    Split the sessions 50/50 between the original random model (one that
    shows two random food images) and a random stratified model (one that
    limits pairings to the same quantile based on click historical click
    through rate). This may help improve the quality of clicks in the future
    as we will be less likely to pair up popular items against unpopular
    items. We'll need to recalculate quantiles and redeploy on a fairly
    regular basis, especially if we add in new food items without much
    history.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,24 +2,37 @@ import * as functions from 'firebase-functions';
 import { Request, Response } from 'express';
 //import * as cors from 'cors';
 import { IMAGES } from './images';
+import { STRATIFIED } from './stratified';
+
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 //const corsHandler = cors({origin: true});
 const cors = require('cors')({origin: true});
-
+function getRandomIndex(setSize: number): number {
+  return Math.floor(Math.random() * setSize);
+}
 function getSession(): number {
-  return Math.floor(Math.random() * 2147483647);
+  return getRandomIndex(2147483647);
 }
 function getRandomFood(otherThan?: number): number {
-  let food = Math.floor(Math.random() * IMAGES.length);    
+  let food = getRandomIndex(IMAGES.length);    
   while(food === otherThan) {
-    food = Math.floor(Math.random() * IMAGES.length);
+    food = getRandomIndex(IMAGES.length);
   }
   return food;
 }
-function log(request: Request, session: number): void {
-  console.log(`${request.ip} ${session} "${request.url}" "${request.headers["user-agent"]}" ${new Date().getTime()}`);
+function getRandomStratifiedFood(otherThan?: Array<number>): Array<number> {
+  const strata: number = otherThan ? otherThan[0] : getRandomIndex(STRATIFIED.length); 
+  const otherFood: number = otherThan ? otherThan[1] : undefined;
+  let food = getRandomIndex(STRATIFIED[strata].length);    
+  while(food === otherFood) {
+    food = getRandomIndex(STRATIFIED[strata].length);    
+  }
+  return [strata, food];
+}
+function log(request: Request, session: number, strategy: string): void {
+  console.log(`${request.ip} ${session} "${request.url}" "${request.headers["user-agent"]}" ${new Date().getTime()} "${strategy}"`);
 }
 export const hunger = functions.https.onRequest((request: Request, response: Response) => {
   cors(request, response, () => {
@@ -27,10 +40,20 @@ export const hunger = functions.https.onRequest((request: Request, response: Res
     if (!session) {
       session = getSession(); 
     }
-    log(request, session);
-    const a=getRandomFood();
-    const b=getRandomFood(a);
-    response.json({"searchSession":session,"a":IMAGES[a],"b":IMAGES[b]});
+
+    // we obviously need to refactor this soon or managing model/strategy
+    // selection will get out of hand
+    if(session % 2) {
+      log(request, session, "random");
+      const a=getRandomFood();
+      const b=getRandomFood(a);
+      response.json({"searchSession":session,"a":IMAGES[a],"b":IMAGES[b]});
+    } else {
+      log(request, session, "stratified");
+      const a=getRandomStratifiedFood();
+      const b=getRandomStratifiedFood(a);
+      response.json({"searchSession":session,"a":STRATIFIED[a[0]][a[1]],"b":STRATIFIED[b[0]][b[1]]});
+    }
   });
 });
 
---
+++ functions/src/mock-images.ts
@@ -0,0 +1,4 @@
+export const IMAGES: string[] = [
+"0000002.jpg",
+"0000999.jpg"
+];
