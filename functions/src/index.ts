39beb399a4384326c1e49b39e616cfd3ac0eb93b
Jeff Hansen <dscheffy@gmail.com>
2019-12-26 23:15:06 +0100

    Hash user identifiers for better anonymity
    It's helpful to know all of the user agent and ip address details, but
    it also comes at the price of less user privacy. Hashing those details
    and only maintaining 20 bits of the hash lets us maintain some linkage
    between sessions, but with limited traceback.
    
    I also updated the firebase cli after getting an email saying that the
    old cli would not be deploying endpoints as publicly accessible. I
    started to fix a number of security audit warnings by upgrading
    everything else while I was at it, but then I couldn't compile the
    typescript due to express cors type errors. I'll figure that out later
    on.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,13 @@
 import * as functions from 'firebase-functions';
 import { Request, Response } from 'express';
+import * as crypto from 'crypto';
 //import * as cors from 'cors';
 
 const cors = require('cors')({origin: true});
 export const appetite = functions.https.onRequest((request: Request, response: Response) => {
   cors(request, response, () => {
-    console.log(`${request.ip} "${request.headers['user-agent']}" ${JSON.stringify(request.body)}`);
+    const segment = crypto.createHash('md5').update(`${request.ip} "${request.headers['user-agent']}"`).digest("hex").substr(0,5);
+    console.log(`${segment} ${JSON.stringify(request.body)}`);
     response.json("thankyou!");
   });
 });
